f90f8a41199d622039ba2b4e20a3502f56977dea
kehayashi <kehayashi@hotmail.com>
2018-01-31 16:23:08 -0200

    AdminLTE

--- resources/views/template_main.blade.php
+++ resources/views/template_main.blade.php
@@ -111,23 +111,23 @@
 <!-- jQuery 2.2.3 -->
 <script async="" src="//www.google-analytics.com/analytics.js"></script>
 
-<script src="{{ asset("bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js") }}"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="{{ asset("bower_components/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
 <!-- SlimScroll -->
-<script src="{{ asset("bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
 <!-- FastClick -->
-<script src="{{ asset("bower_components/AdminLTE/plugins/fastclick/fastclick.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/fastclick/fastclick.js") }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset("bower_components/AdminLTE/dist/js/app.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/dist/js/app.min.js") }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset("bower_components/AdminLTE/dist/js/demo.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/dist/js/demo.js") }}"></script>
 
-<script src="{{ asset("bower_components/AdminLTE/plugins/select2/select2.full.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/select2/select2.full.min.js") }}"></script>
 
-<script src="{{ asset("bower_components/AdminLTE/plugins/datatables/jquery.dataTables.min.js") }}" ></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/datatables/jquery.dataTables.min.js") }}" ></script>
 
-<script src="{{ asset("bower_components/AdminLTE/plugins/datatables/dataTables.bootstrap.min.js") }}" ></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/datatables/dataTables.bootstrap.min.js") }}" ></script>
 
 </body>
 </html>

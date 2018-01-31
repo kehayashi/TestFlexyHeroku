864491a7ece365cbbf0c8194ad4a858e36a77148
kehayashi <kehayashi@hotmail.com>
2018-01-31 16:36:29 -0200

    connection db

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_2b70ebc9205c673'),
+            'username' => env('DB_USERNAME', 'bd8ba5cb76eb5d'),
+            'password' => env('DB_PASSWORD', '4733086a'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
